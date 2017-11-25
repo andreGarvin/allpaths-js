6abe9fa701a623dd94d68d943a79ac990db3b4c1
andreGarvin <andregarvin718@gmail.com>
2017-11-25 14:32:48 -0400

    Major bug clean up for filtering for hidden, extendsion, and ONLY_dir duplication of paths being appended. Also created a abstarctio for filtering the paths called add_to_tree(), added prop path accessibility instead of passing the _path and _opts arguments. Filter the paths before the foor lop and making the paths absolute in the paths array.

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
 # allpaths-js (allpaths)
 #### There is one in GO lang as well
-This is am module for getting all the absolute paths of a given path to a working directory
+This is am module for getting all the absolute resolved file paths of a given path to a working directory
 
-I create this module because node needs a package for recursively getting a file or folders absolute path.
+I created this module because node needs a package for recursively getting all absolute paths for files and folders, also filtering these files/folders as well.
+
+```bash
+npm i allPaths -S
+```
 
 ```js
-allPaths('../', {
-	ext: '.js'
+// This will gather all files/folders that match this file extension
+allPaths('..', {
+	// You can also inlcude a property call 'path' to pass the root path
+	// path: '..'
+	ext: '.js',
+	// excludes all files matching these names
+	excludes: [ '.git', 'node_modules' ]
 })
 .then(paths => console.log(paths))
 .catch(err => console.error(err))
 ```
 
-This function takes in `_opts` Object whihc comes with these properties
+This function takes in `_opts` Object which comes with these properties
 * `ONLY_dir`: Only get paths that are directories.
 * `ext`: Gathers paths with a certain extension.
 * `hidden`: Gather only paths to files that are hidden.
-* `ignores`: A array of files or folders to ignore and not gather its absolute path to.
-
-
+* `excludes`: A array of files or folders to ignore and not gather its absolute path to.
--- allPaths.js
+++ allPaths.js
@@ -3,52 +3,80 @@ const path = require('path')
 
 
 function recursiveDirDive(_path, _opts = {}) {
-    
+    const {
+        excludes = [],
+        hidden = true,
+        ext = '',
+        ONLY_dir = false
+    } = _opts
+
+    function add_to_tree(file_name) {
+        let add_to_tree_boolean = false;
+
+        if (ext === '' && hidden) {
+            add_to_tree_boolean = true
+        }
+        if (ext !== '' && path.extname(file_name) === ext) {
+            add_to_tree_boolean = true
+        }
+        if (hidden && file_name[0] === '.' && ext === '') {
+            add_to_tree_boolean = true
+        } else if (hidden === false && file_name[0] !== '.' && ext === '') {
+            add_to_tree_boolean = true
+        }
+
+        return add_to_tree_boolean
+    }
+
     function dive(root, paths, tree) {
         try {
             // resolves the root path given
             root = path.resolve(root)
-            paths = paths.length !== 0 ? paths : readdirSync(root)
-            
+
+            // filters all the excludes file paths and resolves the path
+            paths = paths.filter(i => {
+                return !excludes.includes(i)
+            }).map(u => path.resolve(root, u))
+
             for (let i in paths) {
                 // joining the root path with the child from the directory
-                const absPath = path.resolve(root, paths[i])
-                const is_path_dir = lstatSync(absPath).isDirectory()
-                
-                const { ignores = [], hidden = true, ext = '', ONLY_dir = false } = _opts
-                if (Object.values(_opts).length == 0) {
-                    tree.push(absPath)
+                const abs_path_basename = path.basename(paths[i])
+                const is_path_dir = lstatSync(paths[i]).isDirectory()
+
+                if (Object.values(_opts).length === 0) {
+                    tree.push(paths[i])
                 } else {
-                    if (!ignores.includes(path.basename(absPath))) {
-                        const absPathBaseName = path.basename(absPath)
-                        if (path.extname(absPathBaseName) === ext) {
-                            tree.push(absPath)
-                        } else if (hidden && !is_path_dir && absPathBaseName[0] == '.') {
-                            tree.push(absPath)
-                        } 
-                        if (is_path_dir) {
-                            
-                            if (hidden && ONLY_dir === false) {
-                                tree.push(absPath)
-                            }
-                            if (ONLY_dir) {
-                                tree.push(absPath)
-                            }
-                            dive(absPath, readdirSync(absPath), tree)
+                    // if it is a DIRECTORY
+                    if (is_path_dir) {
+
+                        if (add_to_tree(abs_path_basename)) {
+                            tree.push(paths[i])
+                        }
+                        dive(paths[i], readdirSync(paths[i]), tree)
+                    } else if (ONLY_dir === false) {
+                        
+                        if (add_to_tree(abs_path_basename)) {
+                            tree.push(paths[i])
                         }
                     }
                 }
             }
             return tree
         } catch (err) {
-            return err
+            return err.message
         }
     }
+
     return dive(_path, readdirSync(_path), [])
 }
 
 module.exports = (_path, _opts) => {
-    _path = _path ? _path : __dirname
+    // if _path is object then assign _path to _opts 
+    if (typeof _path === 'object') {
+        _opts = _path
+        // if _opts.path is given assign to _path elese __dirname
+        _path = _opts.path ? _opts.path : __dirname
+    }
 
     return new Promise((resolve, reject) => {
         const result = recursiveDirDive(_path, _opts)
